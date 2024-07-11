ad0a327fca98cb11aa8e35fb99b18e7bf2637688
yosualr <yosualr12@gmail.com>
2024-07-11 08:41:59 +0700

    up

--- src/main/java/com/tujuhsembilan/example/configuration/ApplicationConfig.java
+++ src/main/java/com/tujuhsembilan/example/configuration/ApplicationConfig.java
@@ -74,6 +74,9 @@ public class ApplicationConfig {
         .authorizeHttpRequests(req -> req
             .requestMatchers(AntPathRequestMatcher.antMatcher("/auth/jwks.json")).permitAll()
             .requestMatchers(AntPathRequestMatcher.antMatcher("/auth/login")).permitAll()
+            .requestMatchers(AntPathRequestMatcher.antMatcher("/sample/data-a")).hasAnyAuthority("ROLE_A")
+            .requestMatchers(AntPathRequestMatcher.antMatcher("/sample/data-b")).hasAnyAuthority("ROLE_B")
+            .requestMatchers(AntPathRequestMatcher.antMatcher("/sample/data-c")).hasAnyAuthority("ROLE_A", "ROLE_B")
             .anyRequest().authenticated())
         // Authorization (DEFAULT IN MEM)
         .userDetailsService(new InMemoryUserDetailsManager(
